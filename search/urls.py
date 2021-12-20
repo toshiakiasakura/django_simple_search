9c9da93b159b6e8b9d63f2ab8b1f750433d93dcd
toshiakiasakura <aflu.blossompaws@gmail.com>
2021-12-20 16:01:19 +0900

    bootstrap settings were added.

--- search/urls.py
+++ search/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "search"
 urlpatterns = [
-  path("", views.IndexView.as_view(), name="index" ),
-  path("all_word", views.AllWordView.as_view(), name="all_word"),
+  #path("", views.IndexView.as_view(), name="index" ),
+  path("", views.AllWordView.as_view(), name="all_word"),
   path("search", views.search_word, name="search_word"),
 ]
